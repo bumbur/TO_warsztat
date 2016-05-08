f27e4d4d4ad670af74c5f46328a604cd57473c80
plompart <lompartpatryk@gmail.com>
2016-05-08 21:32:22 +0200

    test

--- Staff.java
+++ Staff.java
@@ -5,7 +5,7 @@ public class Staff extends UserAbstract{
     OrderDB orderDatabase;
 
     public Staff(String name,String surname,OrderDB orderDatabase) {
-        this.name=name;
+        this.names=name;
         this.surname=surname;
         this.orderDatabase=orderDatabase;
     }
